80d63a775a2dbf15b5729ccbc39efaffd921f3e4
ladymoralesvera <ladymoralesvera@gmail.com>
2026-05-26 19:54:23 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="ISP Analytics Dashboard",
+    page_icon="📡",
+    layout="wide"
+)
+
+st.title("📡 ISP Analytics Dashboard")
+
+st.subheader(
+    "Predicción de abandono de clientes en empresas proveedoras de Internet"
+)
+
+st.write("Proyecto Final Integrador")
+
+st.success("Aplicación creada por Lady Morales Vera")
